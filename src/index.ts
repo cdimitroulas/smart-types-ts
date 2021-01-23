5dcf34b9e5b62811edaaa30f9b1387edc0662268
cdimitroulas <c.dimitroulas@gmail.com>
2021-01-23 19:11:06 +0000

    Fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-export { FiniteArray, mkFiniteArray } from "./array/finiteArray";
+export { ArrayWithLength, mkArrayWithLength } from "./array/arrayWithLength";
 export { Int, mkInt } from "./number/int";
 export { NumInRange, mkNumInRange } from "./number/numInRange";
 export { PositiveInt, mkPositiveInt } from "./number/positiveInt";
 export { PositiveNum, mkPositiveNum } from "./number/positiveNum";
 export { EmailAddress, mkEmailAddress } from "./string/emailAddress";
+export {
+  StringWithLength,
+  mkStringWithLength
+} from "./string/stringWithLength";
 export { URL, mkURL } from "./string/url";
 export { SmartType, SmartTypeRefined } from "./utilTypes";
