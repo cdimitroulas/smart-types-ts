1439861f9120d0f7de5ff76cace4c4ca15dc6b72
cdimitroulas <c.dimitroulas@gmail.com>
2021-01-23 17:34:45 +0000

    Simplify URL type

--- src/string/url.ts
+++ src/string/url.ts
@@ -3,12 +3,12 @@ import { pipe } from "fp-ts/lib/pipeable";
 import validator from "validator";
 import { Never } from "../utilTypes";
 
-export type URL<A extends string = string> = A & Never<"__Url__">;
+export type URL = string & Never<"__Url__">;
 
-export const mkURL = <A extends string = string>(value: A): o.Option<URL<A>> =>
+export const mkURL = (value: string): o.Option<URL> =>
   pipe(
     value,
     s => s.trim(),
     o.fromPredicate(validator.isURL),
-    o.map(id => id as URL<A>)
+    o.map(id => id as URL)
   );
